b11a064e6e96b7b531693c475b2a690e96c59e58
sageibra <akaev_ibragim@mail.ru>
2021-03-11 23:16:25 +0300

    Add constants for font names

---
+++ SnapKitUI/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  SnapKitUI
+//
+//  Created by Ibragim Akaev on 11/03/2021.
+//
+
+import UIKit
+
+enum Font {
+    static let helveticaBold    = "HelveticaNeue-Bold"
+    static let helveticaRegular = "HelveticaNeue-Regular"
+    static let helveticaMedium  = "HelveticaNeue-Medium"
+}
