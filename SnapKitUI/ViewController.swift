ee065489298f1c51bbd7763d39d2f36d55e8b5c4
sageibra <akaev_ibragim@mail.ru>
2021-03-11 17:33:39 +0300

    Initial Commit

---
+++ SnapKitUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SnapKitUI
+//
+//  Created by Ibragim Akaev on 11/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
