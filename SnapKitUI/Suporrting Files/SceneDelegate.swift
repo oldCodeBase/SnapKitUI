5b1f5b203bf2eee06381b252cb2be61797702d6f
sageibra <akaev_ibragim@mail.ru>
2021-03-12 18:58:48 +0300

    Create a NavigationController

--- SnapKitUI/Suporrting Files/SceneDelegate.swift
+++ SnapKitUI/Suporrting Files/SceneDelegate.swift
@@ -13,9 +13,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        let vc = AuthViewController()
+        
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = SignInViewController()
+        window?.rootViewController = UINavigationController(rootViewController: vc)
         window?.makeKeyAndVisible()
     }
 }
