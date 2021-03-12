586d4b6d5662641ccd0ea3d6f2ffab6345e1380b
sageibra <akaev_ibragim@mail.ru>
2021-03-12 18:55:44 +0300

    Clean up code and add targets to transition between screens

--- SnapKitUI/Screens/AuthViewController.swift
+++ SnapKitUI/Screens/AuthViewController.swift
@@ -20,39 +20,54 @@ class AuthViewController: UIViewController {
     private let pictureImageView  = CustomImageView(imageName: "image")
     
     private let signUpButton      = CustomButton(title: "SIGN UP", bgColor: #colorLiteral(red: 0.5568627451, green: 0.5921568627, blue: 0.9921568627, alpha: 1))
-    private let logInButton       = CustomButton(title: "LOG IN", titleColour: #colorLiteral(red: 0.5568627451, green: 0.5921568627, blue: 0.9921568627, alpha: 1))
+    private let logInButton       = CustomButton(title: "LOG IN", titleColour: #colorLiteral(red: 0.5568627451, green: 0.5921568627, blue: 0.9921568627, alpha: 1), cornerRadius: 0)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
         updateUI()
+        configureViewController()
+    }
+    
+    private func configureViewController() {
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    @objc private func signUpButtonTapped() {
+        let vc = SignInViewController()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc private func logInButtonTapped() {
+        let vc = CreateAccountViewController()
+        navigationController?.pushViewController(vc, animated: true)
     }
 }
 
 extension AuthViewController {
     
-    func updateUI() {
+    private func updateUI() {
         view.addSubviews(silentLabel, logoImageView, moonLabel, pictureImageView,
                          quizLabel, descriptionLabel, signUpButton, isRegisteredLabel, logInButton)
         
-        signUpButton.layer.cornerRadius = 30
-        
         isRegisteredLabel.text = "ALREADY HAVE AN ACCOUNT?"
         descriptionLabel.text  = "Thousand of people are using silent moon for small meditation"
+        signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
+        logInButton.addTarget(self, action: #selector(logInButtonTapped), for: .touchUpInside)
         
         logoImageView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
-            make.top.equalToSuperview().offset(80)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
         }
         
         silentLabel.snp.makeConstraints { make in
             make.centerY.equalTo(logoImageView)
-            make.trailing.equalTo(logoImageView.snp.leading).inset(-5)
+            make.trailing.equalTo(logoImageView.snp.leading).offset(-5)
         }
         
         moonLabel.snp.makeConstraints { make in
             make.centerY.equalTo(logoImageView)
-            make.leading.equalTo(logoImageView.snp.trailing).inset(-5)
+            make.leading.equalTo(logoImageView.snp.trailing).offset(5)
         }
         
         pictureImageView.snp.makeConstraints { make in
@@ -63,30 +78,30 @@ extension AuthViewController {
         
         quizLabel.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
-            make.top.equalTo(pictureImageView.snp.bottom).offset(80)
+            make.top.equalTo(pictureImageView.snp.bottom).offset(60)
         }
         
         descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(quizLabel.snp.bottom).offset(15)
             make.leading.equalToSuperview().offset(50)
             make.trailing.equalToSuperview().offset(-50)
-            make.top.equalTo(quizLabel.snp.bottom).offset(15)
         }
         
         signUpButton.snp.makeConstraints { make in
-            make.leading.equalToSuperview().offset(50)
-            make.trailing.equalToSuperview().offset(-50)
+            make.leading.equalToSuperview().offset(30)
+            make.trailing.equalToSuperview().offset(-30)
             make.bottom.equalTo(logInButton.snp.top).inset(-20)
             make.height.equalTo(60)
         }
         
-        isRegisteredLabel.snp.makeConstraints { make in
-            make.leading.equalToSuperview().offset(76)
-            make.bottom.equalToSuperview().inset(46)
+        logInButton.snp.makeConstraints { make in
+            make.leading.equalTo(isRegisteredLabel.snp.trailing).offset(4)
+            make.centerY.equalTo(isRegisteredLabel.snp.centerY)
         }
         
-        logInButton.snp.makeConstraints { make in
-            make.leading.equalTo(isRegisteredLabel.snp.trailing).offset(8)
-            make.bottom.equalToSuperview().inset(40)
+        isRegisteredLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview().offset(-24)
+            make.bottom.equalToSuperview().inset(50)
         }
     }
 }
